95fd36cf01c099e48d04db5bf8dbbc6a2ce1a916
bldrtech <dan@eldotech.com>
2021-02-28 19:30:29 -0700

    bitly specific middleware

--- root/website.py
+++ root/website.py
@@ -1,6 +1,6 @@
 import re
-import collections
-
+import requests
+import json
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from flask.views import MethodView
 
@@ -12,7 +12,6 @@ uploadsPath = '/opt/upload/uploads'
 app = Flask(__name__, static_folder='static')
 app.config['MAX_CONTENT_LENGTH'] = 10485760  # 10 MB
 
-
 class Upload(MethodView):
     @staticmethod
     def get():
@@ -21,32 +20,65 @@ class Upload(MethodView):
     @staticmethod
     def post():
         errors = []
-        output_text = None
-        files = request.files['new_file']
-        if files:
+        links = []
+        output = []
+        token = None
+        #default_guid = ""
+        output_text = ""
+        try:
+            token = request.headers['APP_ACCESS_TOKEN']
+            # errors.append(format(request.headers))
+        except:
+            pass
+        if token is None:
             try:
-                new_file = files.read()
-                data = new_file.decode()
-                text_data = re.split("\W+", data)
-                words_count = len(text_data) - 1
-                coll_counted = collections.Counter(text_data)
-                key_count = len(coll_counted.keys()) - 1
-                if words_count == 0 and key_count == 0:
-                    output_text = "File Received... But it was empty. Word Count:{}, Uniq-Word Count:{}".format(words_count, key_count)
-                else:
-                    output_text = "File Received. Thanks! Word Count:{}, Uniq-Word Count:{}".format(words_count, key_count)
+                token = request.form['access_key']
             except:
-                errors.append("Problem parsing uploaded file. Is it TEXT?")
+                pass
+        if token is not None:
+            # errors.append("Found token:{}".format(token))
+            headers = {'Authorization': token, 'Content-Type': 'application/json'}
+            user_return = requests.get('https://api-ssl.bitly.com/v4/user', headers=headers)
+            #errors.append("UserReturn: {}".format(user_return.text))
+            user_json = json.loads(user_return.text)
+            #errors.append("UserJson: {}".format(user_json))
+            if user_json['default_group_guid'] is not None:
+                default_guid = user_json['default_group_guid']
+                more_records = True
+                links_uri = 'https://api-ssl.bitly.com/v4/groups/' + default_guid + '/bitlinks?size=3'
+                while more_records:
+                    links_return = requests.get(links_uri, headers=headers)
+                    links_json = json.loads(links_return.text)
+                    for link in links_json['links']:
+                        links.append(link['id'])
+                    links_uri = links_json['pagination']['next']
+                    if links_uri is "":
+                        more_records = False
+                if len(links) > 0:
+                    for bit_link in links:
+                        country_uri = 'https://api-ssl.bitly.com/v4/bitlinks/' + bit_link + '/countries?unit=day&units=3&size=100'
+                        country_return = requests.get(country_uri, headers=headers)
+                        country_json = json.loads(country_return.text)
+                        items = country_json['metrics']
+                        if len(items) > 0:
+                            for item in items:
+                                item_detail = item
+                                item_detail['bitlink'] = bit_link
+                                output.append(item_detail)
+                            #errors.append(format(country_json))
+                    if len(output) > 0:
+                        output_text = "{'bitlink_hits_by_country':" + format(output) + "}"
+            else:
+                errors.append("No Group ID available for given access token")
         else:
-            errors.append("Missing File")
+            errors.append("No accesses token provided. Hint: set header ")
         if not errors and output_text is not None:
             return render_template("upload.html", title='File Received', output_text=output_text, basePath=basePath)
         else:
-            return render_template("upload.html", title='File Upload', errors=errors, basePath=basePath)
-
+            return render_template("upload.html", title='File Upload', output_text=output_text, errors=errors,
+                                   basePath=basePath)
 
 app.add_url_rule("/file_upload", view_func=Upload.as_view('users'))
-
 
 @app.route("/<path:path>")
 def send_static_asset(path):
